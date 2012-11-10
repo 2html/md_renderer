a222f6e9082ff3f434319c02e88a1e262d0acdae
Tom Tang <tly1980@gmail.com>
2012-11-11 08:38:21 +1100

    redcarpet render!

---
+++ renderer/render_worker.rb
@@ -0,0 +1,26 @@
+require 'redcarpet'
+require 'zmq'
+require 'json'
+
+context = ZMQ::Context.new(1)
+
+puts "Opening connection pulling task"
+inbound = context.socket(ZMQ::PULL)
+inbound.connect("tcp://127.0.0.1:5555")
+
+puts "Opening connection pushing result"
+outbound = context.socket(ZMQ::PUSH)
+outbound.connect("tcp://127.0.0.1:5556")
+
+markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+        :autolink => true, :space_after_headers => true)
+
+markdown.render("This is *bongos*, indeed.")
+
+loop do
+  msg = inbound.recv
+  task = JSON(msg)
+  task['html'] = markdown.render(task["md_src"])
+  outbound.send JSON(task)
+end
+
